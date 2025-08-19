9f3126eb0a570a60b04595968a00d71adaadb2cc
Rivce06 <gvenegas7978@gmail.com>
2025-08-18 20:05:53 -0600

    Update DevOps_environment_setup.py

--- DevOps_environment_setup.py
+++ DevOps_environment_setup.py
@@ -171,7 +171,7 @@ def install_app(app):
                            "docker-buildx-plugin docker-compose-plugin", shell=True, check=True)
             
             print(f"✅ {app} has been installed successfully.")
-            result = subprocess.run([app, "version"], check=True, capture_output=True, text=True)
+            result = subprocess.run([app, "--version"], check=True, capture_output=True, text=True)
             print(f"Current {app} version: {result.stdout.strip()}")
 
         elif app == "kubectl":
